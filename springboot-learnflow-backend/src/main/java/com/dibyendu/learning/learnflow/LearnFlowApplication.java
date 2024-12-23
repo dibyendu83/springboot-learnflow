19980ce198dbaa15e2ea5e45149a9d8685628316
Dibyendu Rakshit <dibyendu.rakshit@gmail.com>
2024-12-23 20:31:28 +0530

    added initial draft for backend code

---
+++ springboot-learnflow-backend/src/main/java/com/dibyendu/learning/learnflow/LearnFlowApplication.java
@@ -0,0 +1,13 @@
+package com.dibyendu.learning.learnflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFlowApplication.class, args);
+	}
+
+}
